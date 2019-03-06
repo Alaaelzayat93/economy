e2aab8b8528c489adbbbb159c6ccfdc1046a6cfd
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-03-07 01:07:26 +0200

    new

--- js/main.js
+++ js/main.js
@@ -66,19 +66,23 @@ window.onload = function () {
       txt4.style = "display:none";
       txt5.style = "display:none";
 }
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+
+/*        nav   sticky   */
+
+window.onscroll = function () {
+  myFunction()
+};
+
+var navbar = document.getElementById("navbarStick");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
-
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
-
-
 
 
 
