a7ea738cba7595ece184f23b257517353cdcad4d
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-03-06 22:53:15 +0200

    new

--- js/main.js
+++ js/main.js
@@ -22,6 +22,16 @@ $(document).ready(function () {
   });
 
 
+  /*Start   Nice  Scroll*/
+
+$("body").niceScroll({
+  cursorcolor:"#6666FF",
+  cursorwidth:"16px"
+});
+
+  /*End    Nice  Scroll*/
+
+
   /*ACORDION*/
   var acc = document.getElementsByClassName("accordion");
 var i;
@@ -55,6 +65,17 @@ window.onload = function () {
       txt3.style = "display:none";
       txt4.style = "display:none";
       txt5.style = "display:none";
+}
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
 
